bf72c8deaa16961c2827eb46f400b0c9a071ca6b
Kishan Kr Sharma <skishan781@gmail.com>
2021-04-24 11:42:41 +0530

    breaking news done

--- app/src/main/java/com/heycode/freshnewzz/repository/NewsRepository.kt
+++ app/src/main/java/com/heycode/freshnewzz/repository/NewsRepository.kt
@@ -1,8 +1,12 @@
 package com.heycode.freshnewzz.repository
 
+import com.heycode.freshnewzz.api.RetrofitInstance
 import com.heycode.freshnewzz.database.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
 }
--- app/src/main/java/com/heycode/freshnewzz/ui/NewsViewModel.kt
+++ app/src/main/java/com/heycode/freshnewzz/ui/NewsViewModel.kt
@@ -1,9 +1,37 @@
 package com.heycode.freshnewzz.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.heycode.freshnewzz.models.NewsResponse
 import com.heycode.freshnewzz.repository.NewsRepository
+import com.heycode.freshnewzz.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
+
+    // CHANGE COUNTRY CODE FOR LOCAL NEWS
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse((response)))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
